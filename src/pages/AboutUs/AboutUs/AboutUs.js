acfc87fff9116e5a78a7f23aa84aa80960d31554
MdHasanRaju <mdhasanraju2017@gmail.com>
2022-07-15 10:41:58 +0600

    meetTeam section added

--- src/pages/AboutUs/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs/AboutUs.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import ScanApp from '../../Shared/ScanApp/ScanApp';
 import AboutDetails from '../AboutDetails/AboutDetails';
+import MeetTeam from '../MeetTeam/MeetTeam';
 
 const AboutUs = () => {
     return (
         <div>
             <AboutDetails/>
+            <MeetTeam/>
             <ScanApp/>
             <Footer/>
         </div>
--- src/pages/AboutUs/MeetTeam/MeetTeam.js
+++ src/pages/AboutUs/MeetTeam/MeetTeam.js
@@ -1,12 +1,377 @@
-import React from 'react';
-import './MeetTeam.css';
+import React from "react";
+import "./MeetTeam.css";
+import raj from "../../../assets/images/raj-das-Global-Co-Founder-CEO-of-Hirect-India.6a53bd4.png";
+import avishek from "../../../assets/images/abhishek.730e339.png";
+import ramani from "../../../assets/images/ramani.27094df.png";
+import shantanu from "../../../assets/images/shantanu-pandey.46380d9.png";
+import sanche from "../../../assets/images/sanche-joao.f2f1d05.png";
+import popli from "../../../assets/images/abhishek-popli.d7eb6d7.png";
+import rajeev from "../../../assets/images/rajeev-ranjan.089c716.png"; 
+import manoj from "../../../assets/images/manoj-golani.d9b7000.png";
+import vishal from "../../../assets/images/vishal-gupta.f2cc601.png";
+import akanksha from "../../../assets/images/akanksha-tyagi.798b1bf.png";
+import simran from "../../../assets/images/img_simran.3ce6010.png";
 
 const MeetTeam = () => {
-    return (
-        <div>
-            
+  return (
+    <section class="meet-team-section" >
+      <div class="container" >
+        <div class="row" >
+          <div class="col-md-12" >
+            <h1 >Meet The Team</h1>{" "}
+            <div class="meet-team-list" >
+              <ul >
+                <li >
+                  <div class="meet-team-card" >
+                    <div class="team-details" >
+                      <div class="team-img" >
+                        <img
+                          src={raj}
+                          alt=""
+                          
+                        />
+                      </div>{" "}
+                      <div class="team-info" >
+                        <h5 >Raj Das</h5>{" "}
+                        <h6 >
+                          Global Co-Founder and <br  />
+                          CEO of Hirect India
+                        </h6>
+                      </div>
+                    </div>{" "}
+                    <p >
+                      Raj Das is the Global Co-Founder &amp; CEO of Hirect
+                      India, which connects talents to startups without any
+                      middleman. At the company’s helm, Raj is responsible for
+                      overseeing end-to-end business operations, primarily the
+                      management of Product &amp; Marketing in India. An
+                      IIT-Bombay alumnus, Raj brings a rich experience of 3-4
+                      years in mobile internet space and has also mastered
+                      product management and user growth. Through Hirect, Raj
+                      strives to encourage youngsters to join startups and build
+                      wealth for the nation and themselves.
+                    </p>
+                  </div>
+                </li>{" "}
+                <li >
+                  <div class="meet-team-card" >
+                    <div class="team-details" >
+                      <div class="team-img" >
+                        <img
+                          src={avishek}
+                          alt=""
+                          
+                        />
+                      </div>{" "}
+                      <div class="team-info" >
+                        <h5 >Abhishek Singh</h5>{" "}
+                        <h6 >
+                          Senior Vice President - Marketing
+                        </h6>
+                      </div>
+                    </div>{" "}
+                    <p >
+                      Abhishek Singh is the Senior Vice President of Marketing
+                      at Hirect India. In addition to Digital, Creative, and
+                      Communication, he oversees the overall marketing strategy.
+                      He also handles brand positioning and advanced marketing
+                      innovation at Hirect. Mr. Singh has over 14 years of
+                      experience in the media and dot-com industries. As a
+                      marketing expert, he has built and scaled a few of India's
+                      best startups, including Via.com, CommonFloor.com, and
+                      Vakilsearch.com, among others. In addition to being a
+                      master of marketing automation, he is also a proficient
+                      growth hacker.
+                    </p>
+                  </div>
+                </li>{" "}
+                <li >
+                  <div class="meet-team-card" >
+                    <div class="team-details" >
+                      <div class="team-img" >
+                        <img
+                          src={ramani}
+                          alt=""
+                          
+                        />
+                      </div>{" "}
+                      <div class="team-info" >
+                        <h5 >Ramani Ganesh</h5>{" "}
+                        <h6 >
+                          Senior Vice President - Human Resources
+                        </h6>
+                      </div>
+                    </div>{" "}
+                    <p >
+                      Ramani is responsible for directing the business strategy
+                      and human resources for Hirect. She has over fifteen years
+                      of HR experience in IT, import/export, and manufacturing.
+                      She holds a BBM from Bangalore University and MBA from IIM
+                      Calcutta and is certified by HRCI, USA, as a Global
+                      Professional HR (GPHR). Ramani is currently pursuing a
+                      Ph.D. in Human Resources and values continuous learning.
+                      She is also a voracious reader and traveler who believes
+                      that the environment and culture of an organization play
+                      an important role in employee motivation and productivity.
+                    </p>
+                  </div>
+                </li>{" "}
+                <li >
+                  <div class="meet-team-card" >
+                    <div class="team-details" >
+                      <div class="team-img" >
+                        <img
+                          src={shantanu}
+                          alt=""
+                          
+                        />
+                      </div>{" "}
+                      <div class="team-info" >
+                        <h5 >Shantanu Pandey</h5>{" "}
+                        <h6 >
+                          Sr. Vice President - Business Development
+                        </h6>
+                      </div>
+                    </div>{" "}
+                    <p >
+                      Shantanu Pandey is the Senior Vice President, Business
+                      Development at Hirect. He is a B. Tech graduate from Delhi
+                      Technological University (formerly known as Delhi College
+                      of Engineering). He is one of the initial members of the
+                      team, and he is responsible for increasing B2B growth.
+                      Shantanu also heads the strategic alliances at Hirect.
+                      Shantanu has around 3 years of work experience and has
+                      co-founded two other start-ups during his college days. He
+                      also loves to talk about the Indian start-up ecosystem. In
+                      addition, he is a devout feminist who believes in equal
+                      opportunity at work (and everywhere else).
+                    </p>
+                  </div>
+                </li>{" "}
+                <li >
+                  <div class="meet-team-card" >
+                    <div class="team-details" >
+                      <div class="team-img" >
+                        <img
+                          src={popli}
+                          alt=""
+                          
+                        />
+                      </div>{" "}
+                      <div class="team-info" >
+                        <h5 >Abhishek Popli</h5>{" "}
+                        <h6 >
+                          Vice President - Business Development
+                        </h6>
+                      </div>
+                    </div>{" "}
+                    <p >
+                      The Vice President-Business Development at Hirect,
+                      Abhishek Popli, was one of the few early members in charge
+                      of Hirect's offline and online channels, as well as
+                      establishing Hirect's offline presence in PAN India. Mr.
+                      Popli has over 5 years of experience in leadership and
+                      business development positions and is an expert in
+                      increasing the user base by over 50 times in various
+                      HealthTech, EdTech organizations in and outside India
+                      during his stint. He likes to exploit the possibilities of
+                      exploring the untapped market for the growth and expansion
+                      of the organization.
+                    </p>
+                  </div>
+                </li>{" "}
+                <li >
+                  <div class="meet-team-card" >
+                    <div class="team-details" >
+                      <div class="team-img" >
+                        <img
+                         src={sanche}
+                          alt=""
+                          
+                        />
+                      </div>{" "}
+                      <div class="team-info" >
+                        <h5 >Sanche Joao</h5>{" "}
+                        <h6 >Vice President - Operations</h6>
+                      </div>
+                    </div>{" "}
+                    <p >
+                      Sanche Joao, the Vice President-Operations at Hirect
+                      India, is a tenacious leader with a track record of
+                      transforming cultures into disciplined,
+                      performance-oriented teams by instituting a strong sense
+                      of accountability and by driving measurable
+                      process-oriented changes. She has previously spent over 10
+                      years in Dubai working with corporates such as ITP Media
+                      Group where she led several key initiatives across
+                      Operations, Compliance, and Business Development. In her
+                      downtime, Sanche enjoys working on design initiatives. She
+                      is a foodie and is fascinated by micro-cuisines and
+                      exploring travel through food.
+                    </p>
+                  </div>
+                </li>{" "}
+                <li >
+                  <div class="meet-team-card" >
+                    <div class="team-details" >
+                      <div class="team-img" >
+                        <img
+                          src={rajeev}
+                          alt=""
+                          
+                        />
+                      </div>{" "}
+                      <div class="team-info" >
+                        <h5 >Rajeev Ranjan</h5>{" "}
+                        <h6 >
+                          Associate Vice President - Digital Marketing
+                        </h6>
+                      </div>
+                    </div>{" "}
+                    <p >
+                      Rajeev comes with a robust, 13 years of proven track
+                      record in Digital Marketing and Search Marketing with
+                      expertise in End-to-End performance marketing solutions to
+                      drive business growth. When it comes to Online marketing
+                      and analytics, Rajeev possesses strong skill sets in
+                      various verticals including, IT, Real Estate, Banking,
+                      FMCG, Healthcare, E-Commerce, and Education. Rajeev has
+                      also assisted global clientele - in EMEA, North America,
+                      and SEA regions, to build a robust digital presence.
+                    </p>
+                  </div>
+                </li>{" "}
+                <li >
+                  <div class="meet-team-card" >
+                    <div class="team-details" >
+                      <div class="team-img" >
+                        <img
+                         src={manoj}
+                          alt=""
+                          
+                        />
+                      </div>{" "}
+                      <div class="team-info" >
+                        <h5 >Manoj Goplani</h5>{" "}
+                        <h6 >
+                          Associate Vice President - Design
+                        </h6>
+                      </div>
+                    </div>{" "}
+                    <p >
+                      Manoj possesses more than 14 years of robust experience as
+                      a design professional with skillsets in Design -
+                      Advertising, UI/UX Design Solutions, and Product
+                      Development. Manoj has crafted and led corporate
+                      strategies for achieving the top-level goal. He comes with
+                      strong experience in working with business leaders and
+                      clientele from various geographies including EMEA, North
+                      America, and the Middle East across various verticals -
+                      FMCG, E-Commerce, Telecom, Interior Design Space,
+                      Healthcare, and Pharmaceuticals along with Life Sciences
+                      for the brand visibility and enhancing the brand presence
+                      along with product development and operations.
+                    </p>
+                  </div>
+                </li>{" "}
+                <li >
+                  <div class="meet-team-card" >
+                    <div class="team-details" >
+                      <div class="team-img" >
+                        <img
+                          src={vishal}
+                          alt=""
+                          
+                        />
+                      </div>{" "}
+                      <div class="team-info" >
+                        <h5 >Vishal Gupta</h5>{" "}
+                        <h6 >
+                          AVP-Product Management &amp; Business Intelligence
+                        </h6>
+                      </div>
+                    </div>{" "}
+                    <p >
+                      Vishal is currently working as the AVP- Product Management
+                      &amp; Business Intelligence at Hirect. He is a Great Lakes
+                      Alumni and has 7 years of proven track record in Digital
+                      Marketing and Growth Hacking. Vishal has a strong
+                      background in data-driven marketing and decision making in
+                      diverse industries such as IT, Banking, E-Commerce,
+                      FoodTech, and Ed-Tech. With his interest and ability in
+                      solving real-world problems, he always likes to scout out
+                      for new challenges.
+                    </p>
+                  </div>
+                </li>{" "}
+                <li >
+                  <div class="meet-team-card" >
+                    <div class="team-details" >
+                      <div class="team-img" >
+                        <img
+                          src={akanksha}
+                          alt=""
+                          
+                        />
+                      </div>{" "}
+                      <div class="team-info" >
+                        <h5 >Akanksha Tyagi</h5>{" "}
+                        <h6 >Manager-Influencer Marketing</h6>
+                      </div>
+                    </div>{" "}
+                    <p >
+                      Akanksha Tyagi is currently leading the Influencer
+                      Marketing Campaigns at Hirect. She is one of the initial
+                      members of the team. She has extensive expertise in
+                      Content Creation and Marketing, thus facilitating Business
+                      Growth, Customer Acquisition and Retention. She has worked
+                      with 2000+ Influencers, bringing in 50M+ reach monthly.
+                      She has previously worked as a Digital Marketing Intern
+                      with Trell and as a Marketing Intern with Huawei
+                      Technologies India before joining Hirect.
+                    </p>
+                  </div>
+                </li>{" "}
+                <li >
+                  <div class="meet-team-card" >
+                    <div class="team-details" >
+                      <div class="team-img" >
+                        <img
+                          src={simran}
+                          alt=""
+                          
+                        />
+                      </div>{" "}
+                      <div class="team-info" >
+                        <h5 >Simran Saswani</h5>{" "}
+                        <h6 >
+                          Manager- Business Development
+                        </h6>
+                      </div>
+                    </div>{" "}
+                    <p >
+                      Simran Saswani is currently the Manager-Business
+                      Development Department at Hirect. She is one of the
+                      initial members of the team. She has 4 years of work
+                      experience in the Leadership &amp; Mentor profile. After
+                      joining Hirect as a Business Development Intern in 2020,
+                      she progressed to become a Business Development Executive
+                      and an Assistant Manager, and finally to her current
+                      position as the Manager. She is also skilled in
+                      communication, public speaking, teaching, Customer
+                      Relationship Management (CRM), Growth Strategies, Team
+                      Building and Management. A fashion designer by passion,
+                      Simran also loves to travel and explore new adventures.
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default MeetTeam;
